74444b707bd71f518026a705679ce722735fe26b
pixuh <benjacaniu47@gmail.com>
2023-12-03 16:55:12 +0000

    proyecto terminado

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,96 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include <thread>
 #include <vector>
-#include <mutex>
-#include <opencv2/opencv.hpp>
 
-std::mutex mutex;
+void aplicarFiltroLuminosidadSecuencial(cv::Mat& imagen) {
+    for (int i = 0; i < imagen.rows; ++i) {
+        for (int j = 0; j < imagen.cols; ++j) {
+            // Obtener los valores de los canales RGB
+            int canalAzul = imagen.at<cv::Vec3b>(i, j)[0];
+            int canalVerde = imagen.at<cv::Vec3b>(i, j)[1];
+            int canalRojo = imagen.at<cv::Vec3b>(i, j)[2];
 
-void aplicarFiltroLuminosidad(cv::Mat& imagen, int inicio, int fin) {
+            // Calcular el valor de luminosidad (escala de grises)
+            int valorLuminosidad = (canalAzul + canalVerde + canalRojo) / 3;
+
+            // Establecer el mismo valor en los tres canales para escala de grises
+            imagen.at<cv::Vec3b>(i, j)[0] = valorLuminosidad;
+            imagen.at<cv::Vec3b>(i, j)[1] = valorLuminosidad;
+            imagen.at<cv::Vec3b>(i, j)[2] = valorLuminosidad;
+        }
+    }
+}
+
+void aplicarFiltroLuminosidadThreaded(cv::Mat& imagen, int inicio, int fin) {
     for (int i = inicio; i < fin; ++i) {
         for (int j = 0; j < imagen.cols; ++j) {
-            int canalAzul, canalVerde, canalRojo;
+            // Obtener los valores de los canales RGB
+            int canalAzul = imagen.at<cv::Vec3b>(i, j)[0];
+            int canalVerde = imagen.at<cv::Vec3b>(i, j)[1];
+            int canalRojo = imagen.at<cv::Vec3b>(i, j)[2];
 
-            {
-                std::lock_guard<std::mutex> lock(mutex);
-                canalAzul = imagen.at<cv::Vec3b>(i, j)[0];
-                canalVerde = imagen.at<cv::Vec3b>(i, j)[1];
-                canalRojo = imagen.at<cv::Vec3b>(i, j)[2];
-            }
-
+            // Calcular el valor de luminosidad (escala de grises)
             int valorLuminosidad = (canalAzul + canalVerde + canalRojo) / 3;
-            {
-                std::lock_guard<std::mutex> lock(mutex);  // Exclusión mutua para acceso a la matriz
-                imagen.at<cv::Vec3b>(i, j)[0] = valorLuminosidad;
-                imagen.at<cv::Vec3b>(i, j)[1] = valorLuminosidad;
-                imagen.at<cv::Vec3b>(i, j)[2] = valorLuminosidad;
-            }
+
+            // Establecer el mismo valor en los tres canales para escala de grises
+            imagen.at<cv::Vec3b>(i, j)[0] = valorLuminosidad;
+            imagen.at<cv::Vec3b>(i, j)[1] = valorLuminosidad;
+            imagen.at<cv::Vec3b>(i, j)[2] = valorLuminosidad;
         }
     }
 }
 
 int main() {
-    cv::Mat imagen = cv::imread("tu_imagen.jpg");
+    // Cargar la imagen
+    cv::Mat imagen = cv::imread("/jcaniumil/Tarea2-SSOO/src/auto.jpg");
 
     if (imagen.empty()) {
         std::cerr << "No se pudo cargar la imagen." << std::endl;
         return 1;
     }
 
-    int altura = imagen.rows;
+    // Versión secuencial
+    aplicarFiltroLuminosidadSecuencial(imagen);
+
+    // Guardar la imagen procesada
+    cv::imwrite("imagen_procesada_secuencial.jpg", imagen);
+
+    std::cout << "Proceso secuencial completado. Imagen guardada como 'imagen_procesada_secuencial.jpg'" << std::endl;
+
+    // Versión paralela con std::thread
+    cv::Mat imagenThreaded = cv::imread("/jcaniumil/Tarea2-SSOO/src/auto.jpg");
+
+    if (imagenThreaded.empty()) {
+        std::cerr << "No se pudo cargar la imagen para la versión paralela." << std::endl;
+        return 1;
+    }
+
+    // Número de hilos (puedes ajustar esto según el número de núcleos de tu CPU)
     int numHilos = std::thread::hardware_concurrency();
     std::vector<std::thread> hilos;
 
-    int seccion = altura / numHilos;
+    // Dividir la imagen en secciones para cada hilo
+    int seccion = imagenThreaded.rows / numHilos;
     int inicio = 0;
     int fin = seccion;
 
+    // Iniciar los hilos
     for (int i = 0; i < numHilos; ++i) {
-        hilos.emplace_back(aplicarFiltroLuminosidad, std::ref(imagen), inicio, fin);
+        hilos.emplace_back(aplicarFiltroLuminosidadThreaded, std::ref(imagenThreaded), inicio, fin);
         inicio = fin;
         fin += seccion;
     }
 
+    // Esperar a que todos los hilos terminen
     for (auto& hilo : hilos) {
         hilo.join();
     }
 
-    cv::imwrite("imagen_procesada.jpg", imagen);
+    // Guardar la imagen procesada
+    cv::imwrite("imagen_procesada_threaded.jpg", imagenThreaded);
 
-    std::cout << "Proceso completado. Imagen guardada como 'imagen_procesada.jpg'" << std::endl;
+    std::cout << "Proceso paralelo con std::thread completado. Imagen guardada como 'imagen_procesada_threaded.jpg'" << std::endl;
 
     return 0;
 }
